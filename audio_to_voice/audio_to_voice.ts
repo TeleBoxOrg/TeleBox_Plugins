7e50470ea2d97a847ad7176c8216ed0f2adeb285
TiaraBasori <mintabyss233@gmail.com>
2026-05-10 19:42:53 +0800

    fix(audio_to_voice): remove no-op cleanup method (handled by PluginBase)

--- audio_to_voice/audio_to_voice.ts
+++ audio_to_voice/audio_to_voice.ts
@@ -16,9 +16,6 @@ const execAsync = promisify(exec);
 
 
 class AudioToVoicePlugin extends Plugin {
-  cleanup(): void {
-    // 当前插件不持有需要在 reload 时额外释放的长期资源。
-  }
 
   description: string = `🎙️ <b>音频转语音</b><br/><br/>
 <b>命令</b><br/>
